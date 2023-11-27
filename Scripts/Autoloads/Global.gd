4e33fc2b5186455157813794115af0d1b7dd7614
OmniXV0 <OmniXV0@Outlook.com>
2023-11-27 03:28:50 -0600

    Add files via upload

---
+++ Scripts/Autoloads/Global.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var camera = null
+
+var CustomMouseCursor1 = load("res://Sprites/Custom Mouse Cursor.png")
+var CustomMouseCursor2 = load("res://Sprites/Custom Mouse Cursor 2.png")
+
+var level = 1
